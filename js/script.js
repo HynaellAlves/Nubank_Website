64126b5579f46b64a74249fd657b9f7e4c1fab82
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-07-11 15:37:40 -0300

    ajustando mini-game e editando imagens de contexto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
-const nu = document.querySelector(".nu");
+const nu = document.querySelector('.nu');
+const ladrao = document.querySelector('.ladrao')
 
 const pulo = () => {
 
    nu.classList.add('pulo');
+
+   setTimeout(() => {
+
+      nu.classList.remove('pulo');
+
+   }, 700);
 }
+
+const loop = setInterval(() => {
+
+   const ladraoPosition = ladrao.offsetLeft;
+   const nuPosition = +window.getComputedStyle(nu).bottom.replace('px', '');
+
+   if (ladraoPosition <= 228 && ladraoPosition > 140 && nuPosition < 63) {
+
+      ladrao.style.animation = 'none';
+      ladrao.style.left = `${ladraoPosition}px`;
+
+      nu.style.animation = 'none';
+      nu.style.bottom = `${nuPosition}px`;
+
+      nu.src = './img/game-over.png';
+      nu.style.width = '50px';
+
+      clearInterval(loop);
+
+   }
+
+
+}, 10);
 
 document.addEventListener('keydown', pulo);
